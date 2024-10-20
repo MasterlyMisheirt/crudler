6c189038da66759ee723c9b23590da33457b9dfc
MasterlyMisheirt <thewizardofmich@gmail.com>
2024-10-20 18:50:58 +0100

    Revert "Jump to activity 6"
    This reverts commit 91cab9b01fa30cb637c9fd7290c6924562403ff1.

--- src/components/Entity/Modules/ModuleView.js
+++ src/components/Entity/Modules/ModuleView.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
 import FullWidthImage from "react-native-fullwidth-image";
-import Button from "../../UI/button";
 const ModuleView = ({ module }) => {
   // Initialisations ---------------------
   // State -------------------------------
@@ -21,8 +20,6 @@ const ModuleView = ({ module }) => {
           {module.ModuleLeaderName}{" "}
           <Text style={styles.dimText}>(Module Leader)</Text>
         </Text>
-        <Button label="Modify" />
-        <Button label="Delete" />
       </View>
     </View>
   );

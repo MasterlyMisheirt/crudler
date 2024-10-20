3bf142d4a593417ee14fb423ca13151b61d3f1d8
MasterlyMisheirt <thewizardofmich@gmail.com>
2024-10-20 16:44:49 +0100

    Added navigation, passing module to ModuleViewScreen

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -4,17 +4,19 @@ import Screen from "../layout/Screen";
 import initialModules from "../../data/modules.js";
 import ModuleItem from "../Entity/Modules/ModuleItem.js";
 
-export const ModuleListScreen = () => {
+export const ModuleListScreen = ({ navigation }) => {
   //Initialisations -------------
   //State -----------------------
   const [modules, setModules] = useState(initialModules);
   //Handlers --------------------
+  const handleSelect = (module) =>
+    navigation.navigate("ModuleViewScreen", { module });
   const handleDelete = (module) =>
     setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
   //View ------------------------
   return (
     <Screen>
-      <ModuleItem modules={modules} onSelect={handleDelete} />
+      <ModuleItem modules={modules} onSelect={handleSelect} />
     </Screen>
   );
 };
